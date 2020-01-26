c93049fe59da9d8656c630e7fd8b8870741bba28
haolinli96 <lihaolin@u.northwestern.edu>
2020-01-26 00:47:18 -0600

    message content and time fixed

--- js/chatbox.js
+++ js/chatbox.js
@@ -5,11 +5,17 @@ $(() => {
 const sendClickHandler = () => {
     console.log("!!!!!");
     let textContent = document.getElementById("sendText").value;
+    console.log(textContent);
     if(textContent) {
         const outgoingNode = document.getElementsByClassName("outgoing_msg")[0];
         const newOutgoing = outgoingNode.cloneNode(true);
-        console.log(newOutgoing);
+        newOutgoing.childNodes[1].childNodes[1].innerHTML = textContent;
         //newOutgoing.firstChild.firstChild.innerHTML = textContent;
+        let date = new Date();
+        let timeString = (date.getHours() + 1) + ':' + (date.getMinutes() + 1) + ' | '
+         + (date.getMonth() + 1) + '/' + (date.getDate() + 1)
+         console.log(timeString)
+        newOutgoing.childNodes[1].childNodes[3].innerHTML = timeString;
         document.getElementsByClassName("msg_history")[0].appendChild(newOutgoing);
     }
 }
