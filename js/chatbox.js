4e226c15cc069a2c3ddb494361ce389c9d9d0b0e
haolinli96 <lihaolin@u.northwestern.edu>
2020-01-27 23:21:16 -0600

    drag and drop send image preview

--- js/chatbox.js
+++ js/chatbox.js
@@ -42,5 +42,37 @@ const sendClickHandler = (e) => {
 }
 
 const sendFile = (e) => {
+    const dt = e.dataTransfer;
+    const files = dt.files;
 
+    const read = (file) => {
+        const reader = new FileReader();
+        reader.addEventListener('load', function() {
+            let img = document.createElement('img');
+            img.src = reader.result;
+            img.title = file.name;
+            img.classList.add("msg_image");
+            console.log(img.outerHTML)
+
+            const outgoingNode = document.getElementsByClassName("outgoing_msg")[0];
+            const newOutgoing = outgoingNode.cloneNode(true);
+            newOutgoing.childNodes[1].childNodes[1].replaceWith(img)
+            let date = new Date();
+            let timeString = (date.getHours() + 1) + ':' + (date.getMinutes() + 1) + ' | '
+            + (date.getMonth() + 1) + '/' + (date.getDate() + 1)
+            console.log(timeString)
+            newOutgoing.childNodes[1].childNodes[3].innerHTML = timeString;
+            const messageHistory = document.getElementsByClassName("msg_history")[0];
+            messageHistory.appendChild(newOutgoing);
+            messageHistory.scrollTop = messageHistory.scrollHeight;
+        }, false)
+        reader.readAsDataURL(file);
+    }
+    for(let i = 0; i < files.length; ++i){
+        if(files[i].type.startsWith('image/')) {
+            console.log('!!!')
+            read(files[i])
+        }
+    }
+    
 }
